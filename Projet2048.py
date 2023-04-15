2f91bb1866f4c45bffe40bc1f035638a3713c245
AnimSama <animi3993@gmail.com>
2023-04-16 00:22:15 +0200

    Update 2

--- Projet2048.py
+++ Projet2048.py
@@ -1,158 +1,172 @@
-#on importe les module
 import tkinter as tk
 import random
 
-#on commence par definir la fonction principal
-class Game(tk.Frame):
-    
+# Fonction pour ajouter une tuile de manière aléatoire
+def add_random_tile():
+    global grid
+    empty_cells = [(i,j) for i in range(4) for j in range(4) if grid[i][j] == 0]
+    if empty_cells:
+        i, j = random.choice(empty_cells)
+        grid[i][j] = random.choice([2, 4])
 
-    Couleur_grille = "#b8afa9"
-    Couleur_CelluleVide = "#ffd5b5"
-    Police_ScoreLabel = ("Verdana", 24)
-    Police_Score = ("Helvetica", 48, "bold")
-    Police_GameOver = ("Helvetica", 48, "bold")
-    Police_Couleur_GameOver = "#ffffff"
-    Winner_BG = "#ffcc00"
-    Loser_BG = "#a39489" 
+# Fonction pour déplacer les tuiles vers la gauche
+def move_left():
+    global grid
+    for i in range(4):
+        new_row = []
+        merged = []
+        for j in range(4):
+            if grid[i][j] != 0:
+                if not new_row:
+                    new_row.append(grid[i][j])
+                else:
+                    if grid[i][j] == new_row[-1] and (i,j) not in merged:
+                        new_row[-1] *= 2
+                        merged.append((i,j))
+                    else:
+                        new_row.append(grid[i][j])
+        new_row += [0] * (4 - len(new_row))
+        grid[i] = new_row
+    move_left_button = tk.Button(root, text="Déplacer à gauche", width=20, command=move_left)
 
-    Couleur_Cellule = {
-        2: "#fcefe6",
-        4: "#f2e8cb",
-        8: "#f5b682",
-        16: "#f29446",
-        32: "#ff775c",
-        64: "#e64c2e",
-        128: "#ede291",
-        256: "#fce130",
-        512: "#ffdb4a",
-        1024: "#f0b922",
-        2048: "#fad74d"    
-    }
+# Fonction pour déplacer les tuiles vers la droite
+def move_right():
+    global grid
+    for i in range(4):
+        new_row = []
+        merged = []
+        for j in range(3, -1, -1):
+            if grid[i][j] != 0:
+                if not new_row:
+                    new_row.append(grid[i][j])
+                else:
+                    if grid[i][j] == new_row[-1] and (i,j) not in merged:
+                        new_row[-1] *= 2
+                        merged.append((i,j))
+                    else:
+                        new_row.append(grid[i][j])
+        new_row = [0] * (4 - len(new_row)) + new_row
+        grid[i] = new_row
 
-    Couleur_Nombre = {
-        2: "#695c57",
-        4: "#695c57",
-        8: "#ffffff",
-        16: "#ffffff",
-        32: "#ffffff",
-        64: "#ffffff",
-        128: "#ffffff",
-        256: "#ffffff",
-        512: "#ffffff",
-        12048: "#ffffff"
-    }
-
-    Police_Nombre = {
-        2: ("Helvetica", 55, "bold"),
-        4: ("Helvetica", 55, "bold"),
-        8: ("Helvetica", 55, "bold"),
-        16: ("Helvetica", 50, "bold"),
-        32: ("Helvetica", 50, "bold"),
-        64: ("Helvetica", 50, "bold"),
-        128: ("Helvetica", 45, "bold"),
-        256: ("Helvetica", 45, "bold"),
-        512: ("Helvetica", 45, "bold"),
-        1024: ("Helvetica", 40, "bold"),
-        2048: ("Helvetica", 40, "bold"),
-    }    
-
-       #Fonction principal
-    def __init__(self):
-        tk.Frame.__init__(self)
-        self.grid()
-        self.master.title("2048 L1MIASHS ")
-
-        self.grid_main = tk.Frame(
-            self, bg=Game.Couleur_grille, bd=3, width=600, height=600
-        )
-        self.grid_main.grid(pady=(110,0))
-
-        self.GUI_maker()
-        self.start_game()
-
-        self.master.bind("<Gauche>", self.left)
-        self.master.bind("<Droite>", self.right)
-        self.master.bind("<Haut>", self.up)
-        self.master.bind("<Bas>", self.down)
-
-        self.mainloop()
-    
-    #Interface graphique
-    def GUI_maker(self):
-        #création de la grille principale
-        self.cellule = []
+# Fonction pour déplacer les tuiles vers le haut
+def move_up():
+    global grid
+    for j in range(4):
+        new_column = []
+        merged = []
         for i in range(4):
-            row = []
-            for j in range(4):
-                frame_cells = tk.Frame(
-                    self.grid_main,
-                    bg=Game.Couleur_CelluleVide,
-                    width=150,
-                    height=150
-                    
-                )
-                frame_cells.grid(row=i, column=j, padx=5, pady=5)
-                Cellule_Nombre = tk.Label(self.grid_main, bg=Game.Couleur_CelluleVide)
-                Cellule_Data = {"image":frame_cells, "nombre": Cellule_Nombre}
+            if grid[i][j] != 0:
+                if not new_column:
+                    new_column.append(grid[i][j])
+                else:
+                    if grid[i][j] == new_column[-1] and (i,j) not in merged:
+                        new_column[-1] *= 2
+                        merged.append((i,j))
+                    else:
+                        new_column.append(grid[i][j])
+        new_column += [0] * (4 - len(new_column))
+        for i in range(4):
+            grid[i][j] = new_column[i]
 
-                Cellule_Nombre.grid(row=i, column=j)
-                row.append(Cellule_Data)
-            self.cellule.append(row)
+# Fonction pour déplacer les tuiles vers le bas
+def move_down():
+    global grid
+    for j in range(4):
+        new_column = []
+        merged = []
+        for i in range(3, -1, -1):
+            if grid[i][j] != 0:
+                if not new_column:
+                    new_column.append(grid[i][j])
+                else:
+                    if grid[i][j] == new_column[-1] and (i,j) not in merged:
+                        new_column[-1] *= 2
+                        merged.append((i,j))
+                    else:
+                        new_column.append(grid[i][j])
+        new_column = [0] * (4 - len(new_column)) + new_column
+        for i in range(4):
+            grid[i][j] = new_column[i]
 
-        #Creation entête Score
+# Fonction pour sauvegarder une partie en cours dans
+# fichier texte
+def save_game():
+    global grid
+    with open("saved_game.txt", "w") as file:
+        for row in grid:
+            line = ",".join(str(tile) for tile in row)
+            file.write(line + "\n")
+    print("Partie sauvegardée !")
 
-        image_score = tk.Frame(self)
-        image_score.place(relx=0.5, y=60, anchor="center")
-        tk.Label(
-            image_score,
-            text="Score",
-            police=Game.Police_ScoreLabel
-        ).grid(row=0)
-        self.label_score = tk.Label(image_score, text="0", police= Game.Police_Score)
-        self.label_score.grid(row=1)
+# Fonction pour charger une partie enregistrée
+def load_game():
+    global grid
+    with open("saved_game.txt", "r") as file:
+        lines = file.readlines()
+        grid = [[int(tile) for tile in line.strip().split(",")] for line in lines]
+    print("Partie chargée !")
 
-        image_score = tk.Frame(self)
-        image_score.place(relx=0.5, y=60, anchor="center")
-        tk.Label(
-            image_score,
-            text="Score",
-            police=Game.Police_ScoreLabel
-        ).grid(row=0)
-        self.label_score = tk.Label(image_score, text="0", police= Game.Police_Score)
-        self.label_score.grid(row=1)
+# Fonction pour afficher la grille
+def display_grid():
+    global grid
+    for i in range(4):
+        for j in range(4):
+            value = grid[i][j]
+            if value == 0:
+                label = tk.Label(root, text="", width=4, height=2, font=("Arial", 30), relief="solid", borderwidth=1)
+            else:
+                label = tk.Label(root, text=str(value), width=4, height=2, font=("Arial", 30), relief="solid", borderwidth=1)
+            label.grid(row=i+1, column=j)
 
-   #fonction pour lancer le jeux
-    def start_game(self):
-        #creation de matrice avec pour contenant le chiffre zero
-        self.matrice = [[0] * 4 for _ in range(4)]
+# Fonction pour commencer une partie
+def start_game():
+    global grid
+    for i in range(4):
+        for j in range(4):
+            grid[i][j] = 0
+    add_random_tile()
+    add_random_tile()
+    display_grid()
 
-        #Remplir deux cellule au hasard avec deux seconde d'eccart
-        row = random.randint(0,3)
-        col = random.randint(0,3)
-        self.matrice[row][col] = 2
-        self.cellule[row][col]["image"].configure(bg=Game.Couleur_Cellule[2])
-        self.cellule[row][col]["nombre"].configure(
-            bg=Game.Couleur_Cellule[2],
-            fg=Game.Couleur_Nombre[2],
-            police=Game.Police_Nombre[2],
-            text="2"
-        )
-        while(self.matrice[row][col] != 0):
-            row = random.randint(0,3)
-            col = random.randint(0,3)
-        self.matrice[row][col] = 2
-        self.cellule[row][col]["image"].configure(bg=Game.Couleur_Cellule[2])
-        self.cellule[row][col]["nombre"].configure(
-            bg=Game.Couleur_Cellule[2],
-            fg=Game.Couleur_Nombre[2],
-            police=Game.Police_Nombre[2],
-            text="2"
-        )
+# Fonction pour quitter le jeu et afficher le score
+def exit_game():
+    global grid
+    score = sum([sum(row) for row in grid])
+    print("Score : {}".format(score))
+    root.destroy()
 
-        self.score = 0
+# Initialisation de la grille et création de la fenêtre
+grid = [[0]*4 for i in range(4)]
+root = tk.Tk()
+root.title("2048")
 
-def main():
-    Game()
+# Création des boutons
+play_button = tk.Button(root, text="Jouer", command=start_game)
+play_button.grid(row=0, column=0, padx=10, pady=10)
 
-if __name__ == "__main__":
-    main()
+left_button = tk.Button(root, text="Gauche", command=move_left)
+left_button.grid(row=0, column=1, padx=10, pady=10)
+
+right_button = tk.Button(root, text="Droite", command=move_right)
+right_button.grid(row=0, column=2, padx=10, pady=10)
+
+down_button = tk.Button(root, text="Bas", command=move_down)
+down_button.grid(row=0, column=3, padx=10, pady=10)
+
+up_button = tk.Button(root, text="Haut", command=move_up)
+up_button.grid(row=0, column=4, padx=10, pady=10)
+
+save_button = tk.Button(root, text="Sauvegarder", command=save_game)
+save_button.grid(row=0, column=5, padx=10, pady=10)
+
+load_button = tk.Button(root, text="Charger", command=load_game)
+load_button.grid(row=0, column=6, padx=10, pady=10)
+
+exit_button = tk.Button(root, text="Quitter", command=exit_game)
+exit_button.grid(row=0, column=7, padx=10, pady=10)
+
+
+
+
+# Affichage de la fenêtre
+root.mainloop()
